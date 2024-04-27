05d94bebd2e5303765cb1b2fef1c1ec387a5c340
Prayoga K. I. <113886602+Pk2413@users.noreply.github.com>
2024-04-27 22:35:48 +0700

    update + mentahan api

--- app/Http/Controllers/PembuatanSurat.php
+++ app/Http/Controllers/PembuatanSurat.php
@@ -8,8 +8,36 @@ use App\Models\ttd;
 
 class PembuatanSurat extends Controller {
     public static function insert( Request $request ) {
+        $data = $request->except( '_token' );
+
+        $detail_surat = new detail_surat();
+        foreach ( $data as $key => $value ) {
+            $detail_surat->$key = $value;
+        }
+        // dd( $data );
         $kode_surat = $request->input( 'jenisSurat' );
-        return $kode_surat;
+        $ttd = ttd::find( $request->input( 'mengetahui' ) );
+        // dd( $ttd );
+        switch ( $kode_surat ) {
+            case 'skck':
+            return redirect()->route( 'skck' )->with( compact( 'detail_surat', 'ttd' ) );
+            break;
+            case 'surat_ijin':
+                return redirect()->route( 'surat-ijin' )->with( compact( 'detail_surat', 'ttd' ) );
+                break;
+            case 'surat_kematian':
+            return redirect()->route( 'surat-mati' )->with( compact( 'detail_surat', 'ttd' ) );
+            break;
+            case 'surat_penghasilan':
+            return redirect()->route( 'surat-penghasilan' )->with( compact( 'detail_surat', 'ttd' ) );
+            break;
+            case 'sktm':
+            return redirect()->route( 'sktm' )->with( compact( 'detail_surat', 'ttd' ) );
+            break;
+            default:
+            return redirect()->abort( 404 );
+            break;
+        }
     }
 
     public static function preview( Request $request ) {
@@ -28,16 +56,16 @@ class PembuatanSurat extends Controller {
             return redirect()->route( 'skck' )->with( compact( 'detail_surat', 'ttd' ) );
             break;
             case 'surat_ijin':
-            return view( 'surat.surat-ijin' )->with( compact( 'detail_surat', 'ttd' ) );
-            break;
+                return redirect()->route( 'surat-ijin' )->with( compact( 'detail_surat', 'ttd' ) );
+                break;
             case 'surat_kematian':
-            return view( 'surat.surat-mati' )->with( compact( 'detail_surat', 'ttd' ) );
+            return redirect()->route( 'surat-mati' )->with( compact( 'detail_surat', 'ttd' ) );
             break;
             case 'surat_penghasilan':
-            return view( 'surat.surat-penghasilan' )->with( compact( 'detail_surat', 'ttd' ) );
+            return redirect()->route( 'surat-penghasilan' )->with( compact( 'detail_surat', 'ttd' ) );
             break;
             case 'sktm':
-            return view( 'surat.sktm' )->with( compact( 'detail_surat', 'ttd' ) );
+            return redirect()->route( 'sktm' )->with( compact( 'detail_surat', 'ttd' ) );
             break;
             default:
             return redirect()->abort( 404 );
