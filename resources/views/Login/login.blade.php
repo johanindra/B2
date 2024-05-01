852e0fa056d46a75739dcc8c3cbf69c9796a686b
Prayoga K. I. <113886602+Pk2413@users.noreply.github.com>
2024-05-01 13:13:25 +0700

    Update login.blade.php

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -8,8 +8,8 @@
     <link href="{{ asset('https://fonts.googleapis.com/css?family=Montserrat:400,700|Raleway:300,600') }}"
         rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel='stylesheet'
+    href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style1.css') }}">
 
 </head>
